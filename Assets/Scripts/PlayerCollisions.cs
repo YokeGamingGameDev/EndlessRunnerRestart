5d75c956a9c99211deae4d4e9c9f938bc67e244a
MasterFushi <34082227+MasterFushi@users.noreply.github.com>
2020-10-01 21:21:25 -0400

    ScoreScreen and GameOverScreen, Basic Collisions

---
+++ Assets/Scripts/PlayerCollisions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerCollisions : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Coin"))
+        {
+            Destroy(other.gameObject);
+        }
+
+        if(other.CompareTag("Obstacle"))
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+}
